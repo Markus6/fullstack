f588ad3ea35374f68a235e876150e3f49fc6da0c
Markus Makela <markus.makela@tuni.fi>
2019-07-24 23:22:35 +0300

    unicafe step1

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+    const { handleClick, text } = props
+    return (
+        <button onClick={handleClick}>{text}</button>
+    )
+}
+
+const App = () => {
+    // tallenna napit omaan tilaansa
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <Button handleClick={handleGoodClick} text='good' />
+            <Button handleClick={handleNeutralClick} text='neutral' />
+            <Button handleClick={handleBadClick} text='bad' />
+            <h1>Statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
