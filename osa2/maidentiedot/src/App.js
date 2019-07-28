4c9b4840e354c062b1782b9999e6b2ea56103dc7
Markus Makela <markus.makela@tuni.fi>
2019-07-28 23:32:33 +0300

    maidentiedot step3

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -2,10 +2,34 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+const Weather = ({city}) => {
+    const [ weather, setWeather ] = useState([]);
+    const [ weatherIcon, setWeatherIcon ] = useState('');
+
+    const url = `https://api.apixu.com/v1/current.json?key=cd287bb341464ea3910194813192807&q=${city}`;
+
+    useEffect(() => {
+        axios.get(url)
+        .then(response => {
+            setWeather(response.data.current);
+            setWeatherIcon(response.data.current.condition.icon);
+        })
+    }, [url]);
+
+    return (
+        <div>
+            <h2>Weather in {city}</h2>
+            <p>temperature: {weather.temp_c} Celsius</p>
+            <img src={weatherIcon} alt={"Icon for weather condition"} />
+            <p>wind: {weather.wind_kph} kph direction {weather.wind_dir}</p>
+        </div>
+    )
+}
+
 const Country = ({list}) => {
     const { name, capital, population, languages, flag } = list[0]
 
-    const languagesMapped = () => languages.map(lang => <li>{lang.name}</li>)
+    const languagesMapped = () => languages.map(lang => <li key={lang.name}>{lang.name}</li>)
 
     return (
         <div>
@@ -15,6 +39,7 @@ const Country = ({list}) => {
             <h3>languages</h3>
             <ul>{languagesMapped()}</ul>
             <img src={flag} alt={"Country's flag"} width={200} />
+            <Weather city={capital} />
 
         </div>
     )
